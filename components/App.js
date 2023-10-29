db7243ecea8a4d7b56a9bb12e59f668b782ad604
Moe Amaya <moeamaya@gmail.com>
2023-10-29 10:30:33 -0700

    Continue testing existing api

--- components/App.js
+++ components/App.js
@@ -6,43 +6,76 @@ const App = () => {
     <Reascii>
       <GridCount />
       <Row>
-        <Col id="1" fillChar="&nbsp;▒" cols={3} color="#BCFCED" fillColor="#BCFCED">&nbsp;▒</Col>
+        <Col id="1" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
         <Col id="2" cols={6} color="var(--color-brand)">.</Col>
-        <Col id="3" fillChar="&nbsp;▒" cols={3} color="#BCFCED" fillColor="#BCFCED">&nbsp;▒</Col>
+        <Col id="3" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
       </Row>
       <Row>
-        <Col id="1" fillChar="▒&nbsp;" cols={3} color="#BCFCED" fillColor="#BCFCED">▒&nbsp;</Col>
+        <Col id="1" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+        <Col id="2" cols={6} fillChar=" " color="#DA8FDE">.</Col>
+        <Col id="3" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+      </Row>
+      <Row>
+        <Col id="1" fillChar=" ▒ " cols={3} fillColor="var(--color-brand)">Menu</Col>
+        <Col id="2" cols={6} fillChar=" " color="#ECF057">.</Col>
+        <Col id="3" fillChar="&nbsp;▒" cols={3}color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+      </Row>
+      <Row>
+        <Col id="1" fillChar=" ▒ " cols={3} fillColor="var(--color-brand)">↳ 〖 ABOUT</Col>
+        <Col id="2" fillChar=" " cols={6}>. &nbsp;┬─┐┌─┐┌─┐┌─┐┌─┐┬┬ <span style={{color: "#ECF057"}}>JS</span></Col>
+        <Col id="3" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+      </Row>
+      <Row>
+        <Col id="1" fillChar=" ▒ " cols={3} fillColor="var(--color-brand)">↳ ○ GET STARTED</Col>
+        <Col id="2" cols={6} fillChar=" ">. &nbsp;├┬┘├┤ ├─┤└─┐│  &nbsp;││</Col>
+        <Col id="3" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+      </Row>
+      <Row>
+        <Col id="1" fillChar=" ▒ " cols={3} fillColor="var(--color-brand)">↳ ╰ DOCS</Col>
+        <Col id="2" fillChar=" " cols={6}>. &nbsp;┴└─└─┘┴ ┴└─┘└─┘┴┴</Col>
+        <Col id="3" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+      </Row>
+      <Row>
+      <Col id="3" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+        <Col id="2" fillChar=" " cols={6}>. &nbsp;Reascii.js</Col>
+        <Col id="3" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+      </Row>
+      <Row>
+        <Col id="1" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+        <Col id="2" fillChar=" " cols={6} color="#DA8FDE">. &nbsp;Responsive ascii layouts in React</Col>
+        <Col id="3" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+      </Row>
+      <Row>
+        <Col id="1" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+        <Col id="2" cols={6} fillChar=" ">.</Col>
+        <Col id="3" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+      </Row>
+      <Row>
+      <Col id="1" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+        <Col id="2" cols={6} color="var(--color-brand)">.</Col>
+        <Col id="3" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+      </Row>
+      <Row>
+        <Col id="1" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
         <Col id="2" cols={6} fillChar=" " color="#DA8FDE"> </Col>
-        <Col id="3" fillChar="▒&nbsp;" cols={3} color="#BCFCED" fillColor="#BCFCED">▒&nbsp;</Col>
+        <Col id="3" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
       </Row>
       <Row>
-        <Col id="1" fillChar=" ▒ " cols={3} fillColor="#BCFCED">Menu</Col>
-        <Col id="2" cols={6} fillChar=" "color="var(--color-brand)">
-          . REASCII.JS
-        </Col>
-        <Col id="3" fillChar="&nbsp;▒" cols={3}color="#BCFCED" fillColor="#BCFCED">&nbsp;▒</Col>
+        <Col id="1" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+        <Col id="2" cols={6} fillChar=" ">. Text block...</Col>
+        <Col id="3" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+      </Row>
+
+      {/* Section */}
+      <Row>
+        <Col id="1" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
+        <Col id="2" cols={6} fillChar=" " color="#DA8FDE"> </Col>
+        <Col id="3" fillChar="▒&nbsp;" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">▒&nbsp;</Col>
       </Row>
       <Row>
-        <Col id="1" fillChar=" ▒ " cols={3} fillColor="#BCFCED">↳ 〖 ABOUT</Col>
-        <Col id="2" fillChar=" " cols={6} color="var(--color-brand)"> </Col>
-        <Col id="3" fillChar="▒&nbsp;" cols={3} fillColor="#BCFCED">▒&nbsp;</Col>
-      </Row>
-      <Row>
-        <Col id="1" fillChar=" ▒ " cols={3}>↳ ○ GET STARTED</Col>
-        <Col id="2" cols={6} fillChar="›" color="#DA8FDE">
-          . Responsive ascii layouts in React
-        </Col>
-        <Col id="3" fillChar="&nbsp;▒" cols={3}>&nbsp;▒</Col>
-      </Row>
-      <Row>
-        <Col id="1" fillChar=" ▒ " cols={3}>↳ ╰ DOCS</Col>
-        <Col id="2" fillChar=" " cols={6}> </Col>
-        <Col id="3" fillChar="▒&nbsp;" cols={3}>▒&nbsp;</Col>
-      </Row>
-      <Row>
-        <Col id="1" fillChar=" ▒" cols={3}> </Col>
-        <Col id="2" fillChar="." cols={6}> </Col>
-        <Col id="3" fillChar="&nbsp;▒" cols={3}>&nbsp;▒</Col>
+        <Col id="1" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
+        <Col id="2" cols={6} fillChar=" "> </Col>
+        <Col id="3" fillChar="&nbsp;▒" cols={3} color="var(--color-brand)" fillColor="var(--color-brand)">&nbsp;▒</Col>
       </Row>
       <Row>
         <Col id="1" fillChar=" " cols={3}> </Col>
